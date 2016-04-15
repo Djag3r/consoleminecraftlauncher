ca4787f35e33c66e3686b4ccfe83547efe34e3a2
Alex <zeleniq_98@mail.ru>
2016-04-15 19:53:27 +0300

    Goto and few new things

--- Source/consoleminecraftlauncher/Program.cs
+++ Source/consoleminecraftlauncher/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
 
 namespace ConsoleApplication1
 {
@@ -10,50 +6,50 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.WriteLine("");
-                Console.WriteLine("//===== Console Minecraft Launcher =====\\");
-                Console.WriteLine("");
-                Console.WriteLine("Вы можете запустить Minecraft, введя ----->");
-                Console.WriteLine("в строку ниже (1) или (minecraft)");
-                Console.WriteLine("Чтобы узнать информацию о программе, введите (2) или (info)");
-                string multi;
-                multi = Console.ReadLine();
-                if (multi == "1")
-                {
-                    Console.WriteLine("Soon!");
-                    /*Process.Start();*/
-                }
-                if (multi == "minecraft")
-                {
-                    Console.WriteLine("Soon!");
-                    /*Process.Start();*/
-                }
-                if (multi == "2")
-                {
-                    Console.WriteLine("Console Minecraft Launcher");
-                    Console.WriteLine("ver. 0.0.1");
-                    Console.WriteLine("GameFuN, 2011-2015");
-                }
-                if (multi == "info")
-                {
-                    Console.WriteLine("Console Minecraft Launcher");
-                    Console.WriteLine("ver. 0.0.1");
-                    Console.WriteLine("GameFuN, 2011-2015");
-                }
-                /*else
-                {
-                    Console.WriteLine("Не удалось выполнить операцию! Проверьте =====>");
-                    Console.WriteLine("верность введенных вами данных.");
-                }*/
+            start:
+            Console.WriteLine("");
+            Console.WriteLine("//==== Console Minecraft Launcher ====\\");
+            Console.WriteLine("");
+            Console.WriteLine("Вы можете запустить Minecraft, введя ----->");
+            Console.WriteLine("в строку ниже (1) или (minecraft)");
+            Console.WriteLine("Чтобы узнать информацию о программе, введите (2) или (info)");
 
-                Console.WriteLine("Желаете продолжить работу с программой? yes/no");
-                string choose = Console.ReadLine();
-                if (choose == "no")
-                {
-                    Environment.Exit(0);
-                }
+            string multi;
+
+            multi = Console.ReadLine();
+
+            if ((multi == "1") || (multi == "minecraft"))
+            {
+                Console.WriteLine("Soon!");
+                /*Process.Start();*/
+            }
+
+            else if ((multi == "2") || (multi == "info"))
+            {
+                Console.WriteLine("Console Minecraft Launcher");
+                Console.WriteLine("ver. 0.0.1");
+                Console.WriteLine("GameFuN, 2011-2015");
+            }
+
+            else
+            {
+                Console.WriteLine("Не удалось выполнить операцию! Проверьте =====>");
+                Console.WriteLine("верность введенных вами данных.");
+            }
+
+            Console.WriteLine("Желаете продолжить работу с программой? (yes) или (no)");
+
+            string choose = Console.ReadLine();
+
+            if(choose == "yes")
+            {
+                Console.Clear();
+                goto start;
+            }
+
+            else
+            {
+                Environment.Exit(0);
             }
         }
     }
